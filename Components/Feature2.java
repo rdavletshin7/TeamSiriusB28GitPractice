1851ab4e858ffdc54705efcf0caedd17ce8722e3
Ruslan Davletshin <Ruslan.Davletshin1@t-mobile.com>
2023-01-23 17:57:56 -0600

    Created Feature1.java and Feature2.java files. Wrote initial code in both files

---
+++ Components/Feature2.java
@@ -0,0 +1,39 @@
+package Components;
+
+public class Feature2 {
+
+    public static void main(String[] args) {
+
+        getCreditScore();
+        System.out.println(getCreditScore());
+        int score = getCreditScore();
+        System.out.println(score);
+
+
+        System.out.println(hasGoodCreditScore(860));
+        System.out.println(hasGoodCreditScore(700));
+
+        if (hasGoodCreditScore(600)) {
+            System.out.println("GOOD SCORE");
+        } else {
+            System.out.println("BAD SCORE");
+        }
+
+    }
+
+    public static int getCreditScore(){
+        return 800;
+    }
+
+    public static boolean hasGoodCreditScore(int score){
+
+        if(score > 750){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+}
